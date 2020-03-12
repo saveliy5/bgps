49ab9d056ca317804820f501c1d88eaad9882115
Saveliy <SavvaS98@mail.ru>
2020-03-12 23:29:41 +0300

    Initial commit first publication for first lab

---
+++ src/main/java/ru/saveliev/bgps/BgpsApplication.java
@@ -0,0 +1,13 @@
+package ru.saveliev.bgps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BgpsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/saveliev/bgps/BgpsApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.saveliev.bgps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
